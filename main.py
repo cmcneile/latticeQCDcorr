69894711f34b72b951c26b41648f5430b8e162c0
Craig McNeile <craig.mcneile@plymouth.ac.uk>
2020-01-02 20:08:15 +0000

    update

--- main.py
+++ main.py
@@ -5,31 +5,9 @@ import math
 from random import seed
 from random import gauss
 
+# my modules
 import corrlib
-
-##
-##  parameters of the simulation
-##
-
-nt = 8
-ncfg  = 1000
-
-# two smearing functions
-nsmear = 2
-
-#  parameters of the noise
-sig = 0.1 
-
-##
-##
-##
-
-mass = [ 0.4 , 0.6 , 0.9]
-mass_osc = [ 0.5 , 0.65 ]
-
-A = np.array([ [ 1.5 , 1.7 , 1.9 , 0.21 , 0.23   ] ,  [ 1.02 , 1.13 , 1.02 , 0.151 , 0.156   ] ]  )
-
-############################################################
+from simulation import *
 
 print ("Starting to create the correlators")
 print("Length of time direction " , nt)
---
+++ simulation.py
@@ -0,0 +1,24 @@
+##
+##  parameters of the simulation
+##
+import numpy as np
+
+nt = 8
+ncfg  = 1000
+
+# two smearing functions
+nsmear = 2
+
+#  parameters of the noise
+sig = 0.1 
+
+##
+##
+##
+
+mass = [ 0.4 , 0.6 , 0.9]
+mass_osc = [ 0.5 , 0.65 ]
+
+A = np.array([ [ 1.5 , 1.7 , 1.9 , 0.21 , 0.23   ] ,  [ 1.02 , 1.13 , 1.02 , 0.151 , 0.156   ] ]  )
+
+############################################################
